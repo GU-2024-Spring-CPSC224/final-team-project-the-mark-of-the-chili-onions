453b3afcd9f962c2a777499f1d1ccefac827c977
Cash <cashhilstad@gmail.com>
2024-03-26 22:04:59 -0700

    Added Position Class
    Added Position class for representation of positions on the game board.

---
+++ src/main/java/edu/gonzaga/Position.java
@@ -0,0 +1,90 @@
+package edu.gonzaga;
+
+import java.util.Objects;
+
+/**
+ * @author Cash Hilstad
+ * A class that represents a two-dimensional position.
+ */
+public class Position {
+    private int x, y;
+
+    Position(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    Position() {
+        this(0, 0);
+    }
+
+    /**
+     * @return The position's x value.
+     */
+    public int getX() {
+        return x;
+    }
+
+    /**
+     * @return The position's y value.
+     */
+    public int getY() {
+        return y;
+    }
+
+    /**
+     * Sets x to the passed value.
+     * @param x The value to set x to.
+     */
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    /**
+     * Sets y to the passed value.
+     * @param y The value to set y to.
+     */
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    /**
+     * Copies the value of another Position.
+     * @param pos The Position to copy from.
+     */
+    public void copy(Position pos) {
+        setX(pos.getX());
+        setY(pos.getY());
+    }
+
+    /**
+     * @return String representation of the Position.
+     */
+    @Override
+    public String toString() {
+        return "(" + x + "," + y + ")";
+    }
+
+    /**
+     * @param o The object to check for equality.
+     * @return True if and only if this Position and the object have the same values.
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+
+        if (o == null) {
+            return false;
+        }
+
+        if (getClass() != o.getClass()) {
+            return false;
+        }
+
+        Position position = (Position) o;
+
+        return x == position.x && y == position.y;
+    }
+}
---
+++ src/test/java/edu/gonzaga/PositionTest.java
@@ -0,0 +1,46 @@
+package edu.gonzaga;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+public class PositionTest {
+    @Test
+    void equalPositionsAreEqual() {
+        Position a = new Position(1,1);
+        Position b = new Position(1,1);
+
+        Assertions.assertEquals(a, b);
+    }
+
+    @Test
+    void inequalPositionsAreInequal() {
+        Position a = new Position(-1,-1);
+        Position b = new Position(1,1);
+
+        Assertions.assertTrue(!a.equals(b));
+    }
+
+    @Test
+    void stringRepresentationWorks() {
+        Position a = new Position(10,7);
+        String expected = "(10,7)";
+
+        String actual = a.toString();
+
+        Assertions.assertEquals(actual, expected);
+    }
+
+    @Test
+    void copyCreatesEqual() {
+        Position a = new Position(1,1);
+        Position b = new Position();
+
+        b.copy(a);
+
+        Assertions.assertEquals(a, b);
+    }
+}
+
