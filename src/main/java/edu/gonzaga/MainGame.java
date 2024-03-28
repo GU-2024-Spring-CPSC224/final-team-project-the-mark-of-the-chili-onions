8377e2fdb29b8c6b1d613895798183c461c2ab14
Mark Reggiardo <markreggiardo@gmail.com>
2024-03-28 14:09:16 -0700

    Created a ViewController to manage which screen is being displayed, created subclassed JPanels for each screen the program needs to display, created and a method to switch views using ActionEvents.

--- src/main/java/edu/gonzaga/MainGame.java
+++ src/main/java/edu/gonzaga/MainGame.java
@@ -1,24 +1,25 @@
-/*
- * Final project main driver class
- * 
- * 
- * Project Description:
- * 
- * 
- * Contributors:
- * 
- * 
- * Copyright: 2023
+/**
+ * Nuffatafl
+ * CPSC 224, Spring 2024
+ * Final Project
+ * No sources to cite.
+ *
+ * @author Mark Reggiardo
+ * @version v0.1.0 03/28/2024
  */
+
 package edu.gonzaga;
 
+import edu.gonzaga.viewNavigation.ViewController;
 
-/** Main program class for launching your team's program. */
+/** Class that contains the main entry point for program */
 public class MainGame {
+    /**
+     * Main entry point for program
+     * Starts a new game on the singletoon ViewController.main
+     */
     public static void main(String[] args) {
-        System.out.println("Hello Team Game");
-
-
-        // Your code here. Good luck!
+        System.out.println("Program starting");
+        ViewController.main.startGame();
     }
 }
---
+++ src/main/java/edu/gonzaga/views/RulesScreen.java
@@ -0,0 +1,32 @@
+/**
+ * Nuffatafl
+ * CPSC 224, Spring 2024
+ * Final Project
+ * No sources to cite.
+ *
+ * @author Mark Reggiardo
+ * @version v0.1.0 03/28/2024
+ */
+
+package edu.gonzaga.views;
+
+import edu.gonzaga.viewNavigation.ScreenChange;
+
+import javax.swing.*;
+
+/** JPanel that contains the UI for the Rules screen */
+public class RulesScreen extends JPanel {
+    public RulesScreen() {
+        super();
+
+        BoxLayout layout = new BoxLayout(this, BoxLayout.Y_AXIS);
+        this.setLayout(layout);
+
+        JLabel label = new JLabel("Rules Screen");
+        this.add(label);
+
+        JButton goToPreviousViewButton = new JButton("Hide Rules Screen");
+        goToPreviousViewButton.addActionListener(ScreenChange.toPreviousScreen);
+        this.add(goToPreviousViewButton);
+    }
+}
