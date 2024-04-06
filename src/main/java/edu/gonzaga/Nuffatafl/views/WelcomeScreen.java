986c3cba2e2fd1eee909dff9420d6d0576dfd742
Orion Hess <orionhess@gmail.com>
2024-04-06 11:42:47 -0700

    Screen work

--- src/main/java/edu/gonzaga/Nuffatafl/views/WelcomeScreen.java
+++ src/main/java/edu/gonzaga/Nuffatafl/views/WelcomeScreen.java
@@ -12,11 +12,14 @@ package edu.gonzaga.Nuffatafl.views;
 
 import edu.gonzaga.Nuffatafl.viewNavigation.ScreenChange;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import javax.swing.border.Border;
 import java.awt.*;
 
-/** JPanel that contains the UI for the Welcome screen */
+/**
+ * JPanel that contains the UI for the Welcome screen
+ */
 public class WelcomeScreen extends JPanel {
     public WelcomeScreen() {
         super(new BorderLayout());
@@ -27,14 +30,18 @@ public class WelcomeScreen extends JPanel {
     }
 
     private JPanel topPanel() {
-        JPanel topPanel = new JPanel(new FlowLayout());
-        topPanel.add(Box.createHorizontalGlue());
+        JPanel topPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
 
-        JButton showRulesScreenButton = new JButton("Show Rules Screen");
+        JButton showRulesScreenButton = new JButton("Rules");
         showRulesScreenButton.addActionListener(ScreenChange.toRulesScreen);
         topPanel.add(showRulesScreenButton);
 
-        JButton showSettingsScreenButton = new JButton("Show Settings Screen");
+        JButton showSettingsScreenButton = new JButton("Settings");
+        try {
+            showSettingsScreenButton.setIcon(new ImageIcon("./src/main/resources/settings.png"));
+        } catch (Exception ex) {
+            System.out.println(ex);
+        }
         showSettingsScreenButton.addActionListener(ScreenChange.toSettingsScreen);
         topPanel.add(showSettingsScreenButton);
 
@@ -56,7 +63,6 @@ public class WelcomeScreen extends JPanel {
         JButton showGameplayScreenButton = new JButton("Start Game (Show Gameplay screen)");
         showGameplayScreenButton.addActionListener(ScreenChange.toGameplayScreen);
         middlePanel.add(showGameplayScreenButton, gbc);
-
 
 
         middlePanel.setBackground(Color.blue);
