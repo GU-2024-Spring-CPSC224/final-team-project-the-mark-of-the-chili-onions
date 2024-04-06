d930d1bd0bd28f18842e57535f175544057ac49e
Orion Hess <orionhess@gmail.com>
2024-04-06 12:18:05 -0700

    Welcome Screen in usable state, missing function call to set names

--- src/main/java/edu/gonzaga/Nuffatafl/views/WelcomeScreen.java
+++ src/main/java/edu/gonzaga/Nuffatafl/views/WelcomeScreen.java
@@ -10,12 +10,15 @@
 
 package edu.gonzaga.Nuffatafl.views;
 
+import edu.gonzaga.Nuffatafl.viewNavigation.ImageLoading;
 import edu.gonzaga.Nuffatafl.viewNavigation.ScreenChange;
 
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import javax.swing.border.Border;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /**
  * JPanel that contains the UI for the Welcome screen
@@ -24,11 +27,14 @@ public class WelcomeScreen extends JPanel {
     public WelcomeScreen() {
         super(new BorderLayout());
 
-
         this.add(topPanel(), BorderLayout.PAGE_START);
         this.add(middlePanel(), BorderLayout.CENTER);
     }
 
+    /**
+     * Set up the top panel of the welcome screen
+     * @return JPanel top panel
+     */
     private JPanel topPanel() {
         JPanel topPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
 
@@ -37,35 +43,59 @@ public class WelcomeScreen extends JPanel {
         topPanel.add(showRulesScreenButton);
 
         JButton showSettingsScreenButton = new JButton("Settings");
-        try {
-            showSettingsScreenButton.setIcon(new ImageIcon("./src/main/resources/settings.png"));
-        } catch (Exception ex) {
-            System.out.println(ex);
-        }
+        showSettingsScreenButton.setIcon(ImageLoading.getImage("./src/main/resources/settings.png", 20, 20));
+
         showSettingsScreenButton.addActionListener(ScreenChange.toSettingsScreen);
         topPanel.add(showSettingsScreenButton);
 
         return topPanel;
     }
 
+    /**
+     * Set up the middle panel of the welcome screen
+     * @return JPanel middle panel
+     */
     private JPanel middlePanel() {
         JPanel middlePanel = new JPanel(new GridBagLayout());
         GridBagConstraints gbc = new GridBagConstraints();
         gbc.gridx = 0;
         gbc.gridy = 0;
         gbc.fill = GridBagConstraints.HORIZONTAL;
-        gbc.insets = new Insets(10, 10, 10, 10);
+        gbc.insets = new Insets(5, 0, 5, 0);
 
-        JLabel label = new JLabel("Welcome Screen");
+        JLabel label = new JLabel("Nuffatafl");
+        label.setFont(new Font("Serif", Font.PLAIN, 60));
         middlePanel.add(label, gbc);
 
         gbc.gridy++;
+        JLabel player1Label = new JLabel("Player 1 Name");
+        middlePanel.add(player1Label, gbc);
+        gbc.gridx++;
+        JTextField player1Dialog = new JTextField("Player 1");
+        middlePanel.add(player1Dialog, gbc);
+        gbc.gridx--;
+
+        gbc.gridy++;
+        JLabel player2Label = new JLabel("Player 2 Name");
+        middlePanel.add(player2Label, gbc);
+        gbc.gridx++;
+        JTextField player2Dialog = new JTextField("Player 2");
+        middlePanel.add(player2Dialog, gbc);
+        gbc.gridx--;
+
+        gbc.gridy++;
+
         JButton showGameplayScreenButton = new JButton("Start Game (Show Gameplay screen)");
         showGameplayScreenButton.addActionListener(ScreenChange.toGameplayScreen);
+        showGameplayScreenButton.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                // Call something to set names
+            }
+        });
         middlePanel.add(showGameplayScreenButton, gbc);
 
-
-        middlePanel.setBackground(Color.blue);
 
         return middlePanel;
     }
